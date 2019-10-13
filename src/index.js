049d5f9961276c64bd458714fb53a549f544e9f2
Robert Coe <chakrawc@gmail.com>
2019-10-12 17:40:48 -0700

    update mvp-bd-client and log line when ready

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import wss from "./wss.js";
 import hapi from "./hapi.js";
 
 const server = http.createServer();
-
 server.listen( 8080 );
 
 wss( server );
 hapi( server );
+
+console.log( "ready" );
