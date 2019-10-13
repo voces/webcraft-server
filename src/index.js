f9a9694a138e23f32933d0e0dd2f4510ce702356
Robert Coe <chakrawc@gmail.com>
2019-10-13 16:54:18 -0700

    re-enable hapi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 import http from "http";
 import wss from "./wss.js";
-// import hapi from "./hapi.js";
+import hapi from "./hapi.js";
 
 const server = http.createServer();
 server.listen( 8080 );
 
 wss( server );
-// hapi( server );
+hapi( server );
 
 console.log( "ready" );
